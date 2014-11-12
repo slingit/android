23cfa11969e6dd938e68231551e80db223cc950b
Jamie Hoyle <j@jamiehoyle.com>
2014-11-12 14:59:50 +0000

    Fancy titles for set up

--- app/src/main/java/me/boopit/boopapp/MainActivity.java
+++ app/src/main/java/me/boopit/boopapp/MainActivity.java
@@ -1,9 +1,13 @@
 package me.boopit.boopapp;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -12,15 +16,23 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // initial toolbar
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+
+        toolbar.setTitle("Set up");
+        toolbar.setTitleTextAppearance(this, R.style.Theme_BoopSetUpTheme_Title);
+
+        // Set navigation icon
+        //toolbar.setNavigationIcon(R.drawable.ic_launcher);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Navigation", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
